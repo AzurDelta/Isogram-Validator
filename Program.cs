4aa670786247e53f045fec53c19a1efd867d9faf
AzurDelta <Rotar.zahar31@gmail.com>
2024-05-28 15:04:04 +0300

    isogram validator done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿start:
+Console.WriteLine("enter string to validate");
+string stringToValidate = Console.ReadLine();
+
+if (stringToValidate == null)
+    goto start;
+
+if (stringToValidate.Length == stringToValidate.Distinct().Count())
+    Console.WriteLine("string is isogram");
+else
+    Console.WriteLine("string is not isogram");
